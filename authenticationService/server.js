8b5e5aa43bcc6478273ad674fdd88353a8551d6e
Abulrahman <abdul.ashref333@gmail.com>
2021-08-26 09:35:32 +0200

    update server

--- authenticationService/server.js
+++ authenticationService/server.js
@@ -1,13 +1,19 @@
 'use strict'
 require('dotenv').config();
-require('./db/mongoose.js');
+require('./db/connection.js');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const user = require('./routes/users');
+const auth = require('./routes/auth');
 
 const port = process.evn || 3000;
 const app = express();
 app.use(bodyParser.json());
+// app.use(express.urlencoded({extended:false}));
+
+app.use('/api/auth', auth);
+app.use('/api/users', user);
 
 app.listen(port, () => {
   console.log(`http://loacalhost:${port}`);
