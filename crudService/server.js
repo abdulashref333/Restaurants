21db62ccbb37c8d0ee98cffed78c9bda7c711f80
Abulrahman <abdul.ashref333@gmail.com>
2021-08-26 12:32:04 +0200

    setup server and mongodb connection

---
+++ crudService/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+require('./db/connection.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const Resturant = require('./routes/resturant');
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+
+app.use('/api/resturants', Resturant);
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+})
