c99579da42a45633e01ba0c5e12ccb67497175ac
Abulrahman <abdul.ashref333@gmail.com>
2021-08-27 06:50:40 +0200

    modify authe service

--- angularProject/src/app/services/auth.service.ts
+++ angularProject/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {  tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { Observable, of } from 'rxjs';
 ;
 
 const API_URL = environment.authenticationServiceUrl;
@@ -29,15 +30,12 @@ export class AuthService{
         .set('content-type','application/json')
         .set('Accept','application/json')
         .set('authorization','Bearer ' + token.token)
-      // headers=headers.append('Access-Control-Allow-Origin', '*')
-      // headers=headers.append('content-type','application/x-www-form-urlencoded')
 			return headers;
 		} else {
-
-			return {
-				'Content-Type': 'application/json',
-				Accept: 'application/json',
-			};
+      let headers = new HttpHeaders()
+      .set('content-type','application/json')
+      .set('Accept','application/json')
+			return headers
 		}
 	}
 
@@ -58,63 +56,21 @@ export class AuthService{
   signUp(user:any){
     this.http.post<any>(`${API_URL}/users/signup`, user).subscribe(res => console.log(res.result));
   }
-	// getUserByToken(): Observable<any> {
-	// 	const userTokenStr = localStorage.getItem(API_TOKEN);
-	// 	const userToken = JSON.parse(userTokenStr);
-	// 	return this.http.get(this.baseUrl + '/auth/me', {
-	// 		headers: this.authorization()
-	// 	}).pipe(
-	// 		catchError(err => of(null)),
-	// 		tap(e => {
-	// 			if (e) {
-	// 				e.role = userToken.role;
-	// 				this._userRole.next(e.role);
-	// 			}
-	// 		})
-	// 	);
-	// }
 
-	// getToken(token: string, username: string) {
-	// 	return this.http.post<any>(this.baseUrl + this.url + '/token', {
-	// 		token,
-	// 		username
-	// 	})
-	// }
+	logOut(): Observable<any> {
+		const userTokenStr = String(localStorage.getItem(API_TOKEN));
+		const httpHeaders = new HttpHeaders();
+		const userToken = JSON.parse(userTokenStr);
+		localStorage.removeItem(API_TOKEN);
+		httpHeaders.set('authorization', 'Bearer ' + userToken.token);
+		return of({});
+	}
 
-
-
-
-
-	// register(user: User): Observable<any> {
-	// 	return this.http.post<User>(API_USERS_URL + `/auth/signup`, user, { headers: this.authorization() })
-	// 		.pipe(
-	// 			map((res: User) => {
-	// 				return res;
-	// 			})
-	// 		);
-	// }
-
-	// logOut(): Observable<any> {
-	// 	const userTokenStr = localStorage.getItem(API_TOKEN);
-	// 	const httpHeaders = new HttpHeaders();
-	// 	const userToken = JSON.parse(userTokenStr);
-	// 	localStorage.removeItem(API_TOKEN);
-	// 	httpHeaders.set('authorization', 'Bearer ' + userToken.token);
-	// 	return of({});
-	// }
-
-
-	// get currentUserType() {
-	// 	const userTokenStr = localStorage.getItem(API_TOKEN);
-	// 	const userToken = JSON.parse(userTokenStr);
-	// 	return userToken && userToken.role ? userToken.role : '';
-	// }
-
-	// get userId() {
-	// 	const userTokenStr = localStorage.getItem(API_TOKEN);
-	// 	const userToken = JSON.parse(userTokenStr);;
-	// 	return userToken && userToken.userId ? userToken.userId : '';
-	// }
+	get userId() {
+		const userTokenStr = String(localStorage.getItem(API_TOKEN));
+		const userToken = JSON.parse(userTokenStr);;
+		return userToken && userToken.userId ? userToken.userId : '';
+	}
 
 
 	isEmail(value:string): boolean {
