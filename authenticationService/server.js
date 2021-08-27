05e85544dda1613968c2ba58c0a632e7a441d32b
Abulrahman <abdul.ashref333@gmail.com>
2021-08-27 22:55:33 +0200

    edit cors

--- authenticationService/server.js
+++ authenticationService/server.js
@@ -12,24 +12,12 @@ const user = require('./routes/users');
 const auth = require('./routes/auth');
 const port = process.evn || 3000;
  
-const whitelist = ['http://localhost:4200', 'http://localhost:3001']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-// app.use(cors(corsOptions))
 app.use(cors());
 app.use(bodyParser.json());
-// app.use(express.urlencoded({extended:false}));
 
 app.use('/api/auth', auth);
 app.use('/api/users', user);
 
 app.listen(port, () => {
-  console.log(`http://loacalhost:${port}`);
+  console.log(`http://localhost:${port}`);
 })
--- crudService/server.js
+++ crudService/server.js
@@ -7,6 +7,7 @@ const app = express();
 const cors = require('cors')
 
 const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(bodyParser.json());
 
